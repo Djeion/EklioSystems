c6336a662d1dc317b456b4412baca70f5332586b
TOBYONE667 <113035644+TOBYONE667@users.noreply.github.com>
2025-03-14 16:11:39 +0100

    Update Dashboard.tsx

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -56,7 +56,7 @@ function Dashboard() {
                     mode: 'cors',
                     headers: {
                         "Authorization": `Bearer ${IdToken}`,
-                        "Access-Control-Allow-Origin": "*",
+                        
                         "Content-Type": "application/json"
                     },
                     credentials: 'include'
